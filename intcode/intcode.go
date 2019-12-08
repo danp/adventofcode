5df71ce81093f0e860e290b141dbcd5da0655e99
Dan Peterson <dpiddy@gmail.com>
2019-12-07 21:35:30 -0400

    intcode: move work to ops

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -11,53 +11,112 @@ type op struct {
 	name string
 	code int
 	pc   int
+	x    func(*vm) error
 }
+
+var halt = errors.New("halt")
 
 var ops = map[int]op{
 	1: {
 		name: "add",
 		code: 1,
 		pc:   3,
+		x: func(v *vm) error {
+			v.set(v.val(2), v.mval(0)+v.mval(1))
+			return nil
+		},
 	},
 	2: {
 		name: "mult",
 		code: 2,
 		pc:   3,
+		x: func(v *vm) error {
+			v.set(v.val(2), v.mval(0)*v.mval(1))
+			return nil
+		},
 	},
 	3: {
 		name: "input",
 		code: 3,
 		pc:   1,
+		x: func(v *vm) error {
+			if v.input == nil {
+				return errors.New("program wants input but no input func provided")
+			}
+			in, err := v.input()
+			if err != nil {
+				return err
+			}
+			v.set(v.val(0), in)
+			return nil
+		},
 	},
 	4: {
 		name: "output",
 		code: 4,
 		pc:   1,
+		x: func(v *vm) error {
+			if v.output == nil {
+				return errors.New("program wants to output but no output func provided")
+			}
+			return v.output(v.mval(0))
+		},
 	},
 	5: {
 		name: "jump-if-true",
 		code: 5,
 		pc:   2,
+		x: func(v *vm) error {
+			if v.mval(0) > 0 {
+				v.jump(v.mval(1))
+			}
+			return nil
+		},
 	},
 	6: {
 		name: "jump-if-false",
 		code: 6,
 		pc:   2,
+		x: func(v *vm) error {
+			if v.mval(0) == 0 {
+				v.jump(v.mval(1))
+			}
+			return nil
+		},
 	},
 	7: {
 		name: "less-than",
 		code: 7,
 		pc:   3,
+		x: func(v *vm) error {
+			var res int
+			if v.mval(0) < v.mval(1) {
+				res = 1
+			}
+			v.set(v.val(2), res)
+			return nil
+		},
 	},
 	8: {
 		name: "equals",
 		code: 8,
 		pc:   3,
+		x: func(v *vm) error {
+			var res int
+			if v.mval(0) == v.mval(1) {
+				res = 1
+			}
+			v.set(v.val(2), res)
+			return nil
+		},
 	},
 	99: {
 		name: "halt",
 		code: 99,
 		pc:   0,
+		x: func(v *vm) error {
+			return halt
+		},
 	},
 }
 
@@ -73,81 +132,79 @@ const (
 	immediate pmode = 2
 )
 
-// Run runs program, calling input when input is requested and
-// calling output when output is requested.
-// The contents of program may be modified.
-func Run(program []int, input func() (int, error), output func(int) error) error {
-	var pos int
+type vm struct {
+	program []int
+	pos     int
+	ins     instruction
 
-	for pos <= len(program) {
-		inst, err := parseInstruction(program[pos])
-		if err != nil {
+	input  func() (int, error)
+	output func(int) error
+
+	jumped bool
+}
+
+func (v *vm) run() error {
+	for v.pos <= len(v.program) {
+		if err := v.stepInstruction(); err != nil {
 			return err
 		}
 
-		val := func(i int) int {
-			return program[pos+1+i]
+		if err := v.ins.op.x(v); err != nil {
+			if err == halt {
+				err = nil
+			}
+			return err
 		}
-
-		mval := func(i int) int {
-			v := val(i)
-			if inst.pmodes[i] == position {
-				v = program[v]
-			}
-			return v
-		}
-
-		switch inst.op.code {
-		case 1:
-			program[val(2)] = mval(0) + mval(1)
-		case 2:
-			program[val(2)] = mval(0) * mval(1)
-		case 3:
-			if input == nil {
-				return errors.New("program wants input but no input func provided")
-			}
-			in, err := input()
-			if err != nil {
-				return err
-			}
-			program[val(0)] = in
-		case 4:
-			if output == nil {
-				return errors.New("program wants to output but no output func provided")
-			}
-			if err := output(mval(0)); err != nil {
-				return err
-			}
-		case 5:
-			if mval(0) > 0 {
-				pos = mval(1)
-				continue
-			}
-		case 6:
-			if mval(0) == 0 {
-				pos = mval(1)
-				continue
-			}
-		case 7:
-			var res int
-			if mval(0) < mval(1) {
-				res = 1
-			}
-			program[val(2)] = res
-		case 8:
-			var res int
-			if mval(0) == mval(1) {
-				res = 1
-			}
-			program[val(2)] = res
-		case 99:
-			return nil
-		}
-
-		pos += 1 + inst.op.pc
 	}
 
 	panic("got here")
+}
+
+func (v *vm) stepInstruction() error {
+	if v.ins.op.code > 0 && !v.jumped {
+		v.pos += v.ins.op.pc
+	}
+	v.jumped = false
+
+	ins, err := parseInstruction(v.val(0))
+	if err != nil {
+		return err
+	}
+
+	v.ins = ins
+	v.pos++
+
+	return nil
+}
+
+func (v *vm) val(i int) int {
+	return v.program[v.pos+i]
+}
+
+func (v *vm) mval(i int) int {
+	val := v.val(i)
+	if v.ins.pmodes[i] == position {
+		val = v.program[val]
+	}
+	return val
+}
+
+func (v *vm) set(i, val int) {
+	v.program[i] = val
+}
+
+func (v *vm) jump(pos int) {
+	v.pos = pos
+	v.jumped = true
+}
+
+// Run runs program, calling input when input is requested and
+// calling output when output is requested.
+//
+// The contents of program may be modified.
+func Run(program []int, input func() (int, error), output func(int) error) error {
+	vm := &vm{program: program, input: input, output: output}
+	return vm.run()
 }
 
 // Parse takes a program string in the form `1,2,3,...` and returns a
