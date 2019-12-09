84accd9b58ce2dc09a5ded1219877df027d80839
Dan Peterson <dpiddy@gmail.com>
2019-12-09 13:45:49 -0400

    day09: add

---
+++ cmd/day09/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/danp/adventofcode2019/intcode"
+)
+
+func main() {
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	program, err := intcode.Parse(string(b))
+	if err != nil {
+		panic(err)
+	}
+
+	mem := make([]int, 2048)
+	var in int
+
+	switch os.Args[1] {
+	case "boost":
+		in = 1
+	case "distress":
+		in = 2
+	}
+
+	input := func() (int, error) { return in, nil }
+	output := func(x int) error { fmt.Println(x); return nil }
+
+	if err := intcode.Run(program, mem, input, output); err != nil {
+		panic(err)
+	}
+}
