332787405e6025c151a1931bcf769f0c59ab10ba
Dan Peterson <danp@danp.net>
2020-12-08 20:34:29 -0400

    day07: cache tree limbs

--- 2020/cmd/day07/main.go
+++ 2020/cmd/day07/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -64,23 +65,49 @@ func main() {
 
 	fmt.Println(len(seen), "bag colors can eventually contain shiny gold")
 
-	q = []string{"shiny gold"}
-	var bags int
-	for len(q) > 0 {
-		k := q[0]
-		q = q[1:]
+	cache := make(map[string]int)
+	pq := [][]string{{"shiny gold"}}
+	var iters int
+	for len(pq) > 0 {
+		iters++
+		// process longest paths first
+		sort.Slice(pq, func(i, j int) bool { return len(pq[i]) > len(pq[j]) })
+		path := pq[0]
+		k := path[len(path)-1]
+		pq = pq[1:]
 
 		down := downgraph[k]
 		if len(down) == 0 {
+			fmt.Println(path, "is leaf")
+			cache[k] = 0
 			continue
 		}
+		allcached := true
+		var kbags int
 		for o, qty := range down {
-			for i := 0; i < qty; i++ {
-				q = append(q, o)
+			cc, ok := cache[o]
+			if ok {
+				kbags += qty + (qty * cc)
+				continue
 			}
-			bags += qty
+
+			fmt.Println(path, o, "cache miss")
+			allcached = false
+			newp := make([]string, len(path))
+			copy(newp, path)
+			newp = append(newp, o)
+			pq = append(pq, newp)
+		}
+
+		if allcached {
+			fmt.Println(path, "now cached with", kbags, "bags")
+			cache[k] = kbags
+		} else {
+			// put ourselves back in the queue so by the time we come
+			// back around the items not in the cache are filled in
+			pq = append(pq, path)
 		}
 	}
 
-	fmt.Println(bags, "bags are required inside shiny gold")
+	fmt.Println(cache["shiny gold"], "bags are required inside shiny gold, iters:", iters)
 }
