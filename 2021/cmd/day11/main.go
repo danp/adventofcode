d009c4f56aa2263d360fc65f5f80e941c222e6dd
Dan Peterson <danp@danp.net>
2021-12-19 15:43:27 -0400

    wip

---
+++ 2021/cmd/day11/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/danp/adventofcode/scaffold"
+)
+
+func main() {
+	lines := scaffold.Lines()
+
+	grid := make(map[image.Point]int)
+	for y, l := range lines {
+		for x, c := range l {
+			grid[image.Pt(x, y)] = int(c) - '0'
+		}
+	}
+
+	var totalFlashes int
+	for i := 0; i < 100; i++ {
+		newGrid, flashes := step(grid)
+		totalFlashes += flashes
+		grid = newGrid
+	}
+
+	fmt.Printf("totalFlashes: %v\n", totalFlashes)
+}
+
+func step(grid map[image.Point]int) (map[image.Point]int, int) {
+	newGrid := make(map[image.Point]int)
+	flashers := make(map[image.Point]struct{})
+	todo := make(map[image.Point]struct{})
+	for pt, l := range grid {
+		nl := l + 1
+		newGrid[pt] = nl
+		if nl > 9 {
+			flashers[pt] = struct{}{}
+			todo[pt] = struct{}{}
+		}
+	}
+
+	for len(todo) > 0 {
+		for pt := range todo {
+			delete(todo, pt)
+
+			for _, npt := range neigbs(pt) {
+				l, ok := newGrid[npt]
+				if !ok {
+					continue
+				}
+				nl := l + 1
+				if nl > 9 {
+					if _, ok := flashers[npt]; !ok {
+						flashers[npt] = struct{}{}
+						todo[npt] = struct{}{}
+					}
+				}
+				newGrid[npt] = nl
+			}
+		}
+	}
+
+	for pt := range flashers {
+		newGrid[pt] = 0
+	}
+
+	return newGrid, len(flashers)
+}
+
+func neigbs(pt image.Point) []image.Point {
+	dirs := []image.Point{
+		{0, -1},  // n
+		{1, -1},  // ne
+		{1, 0},   // e
+		{1, 1},   // se
+		{0, 1},   // s
+		{-1, 1},  // sw
+		{-1, 0},  // w
+		{-1, -1}, // nw
+	}
+
+	var pts []image.Point
+	for _, d := range dirs {
+		pts = append(pts, pt.Add(d))
+	}
+	return pts
+}
