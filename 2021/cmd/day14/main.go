616530fbc663888d115818c253727e9d7bc963ae
Dan Peterson <danp@danp.net>
2021-12-20 15:39:36 -0400

    wip

--- 2021/cmd/day14/main.go
+++ 2021/cmd/day14/main.go
@@ -11,7 +11,10 @@ import (
 func main() {
 	lines := scaffold.Lines()
 
-	tmpl := lines[0]
+	tmpl := make(map[string]int)
+	for _, p := range pairs(lines[0]) {
+		tmpl[p]++
+	}
 
 	rules := make(map[string]string)
 	for _, l := range lines[2:] {
@@ -19,21 +22,14 @@ func main() {
 		rules[a] = b
 	}
 
-	fmt.Printf("tmpl: %v\n", tmpl)
-	for i := 0; i < 4; i++ {
-		tmpl = step(tmpl, rules)
-		n := 100
-		if n > len(tmpl) {
-			n = len(tmpl)
-		}
-		fmt.Printf("tmpl: %v\n", tmpl[:n])
-	}
-
 	counts := make(map[string]int)
-	for _, c := range tmpl {
-		counts[string(c)]++
+
+	fmt.Printf("tmpl: %v\n", tmpl)
+	for i := 0; i < 10; i++ {
+		tmpl, counts = step(tmpl, counts, rules)
+		fmt.Printf("tmpl: %v\n", tmpl)
+		fmt.Printf("counts: %v\n", counts)
 	}
-	fmt.Printf("counts: %v\n", counts)
 
 	var cs []string
 	for c := range counts {
@@ -47,15 +43,31 @@ func main() {
 	fmt.Printf("most: %v / %v least: %v / %v diff: %v\n", last, counts[last], cs[0], counts[cs[0]], counts[last]-counts[cs[0]])
 }
 
-func step(tmpl string, rules map[string]string) string {
-	var n string
-	for i, p := range pairs(tmpl) {
-		if i == 0 {
-			n += string(p[0])
+func step(tmpl map[string]int, counts map[string]int, rules map[string]string) (map[string]int, map[string]int) {
+	out := make(map[string]int)
+	for p := range tmpl {
+		out[string(p[0])+rules[p]]++
+		out[rules[p]+string(p[1])]++
+
+		v := counts[string(p[0])]
+		if v == 0 {
+			v = 1
 		}
-		n += rules[p] + string(p[1])
+		counts[string(p[0])] = v + out[string(p[0])+rules[p]]
+		v = counts[rules[p]]
+		if v == 0 {
+			v = 1
+		}
+		counts[rules[p]] = v + out[string(p[0])+rules[p]]
+
+		v = counts[string(p[1])]
+		if v == 0 {
+			v = 1
+		}
+		counts[string(p[1])] = v + out[rules[p]+string(p[1])]
+		counts[rules[p]] += out[rules[p]+string(p[1])]
 	}
-	return n
+	return out, counts
 }
 
 func pairs(t string) []string {
